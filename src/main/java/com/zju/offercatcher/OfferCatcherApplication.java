f8766b47dd0f754833944d0ab743779312f17eb7
LiuCY <liuchenyu222@163.com>
2026-04-27 01:48:58 +0800

    init: 初始化仓库

---
+++ src/main/java/com/zju/offercatcher/OfferCatcherApplication.java
@@ -0,0 +1,13 @@
+package com.zju.offercatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferCatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfferCatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zju/offercatcher/OfferCatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zju.offercatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfferCatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
