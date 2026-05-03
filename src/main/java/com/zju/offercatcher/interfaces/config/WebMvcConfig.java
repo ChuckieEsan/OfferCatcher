e7f354205a25d192c5eacedcd63c1f0180eb0b90
LiuCY <liuchenyu222@163.com>
2026-05-03 15:04:15 +0800

    fix: 优化了跨域配置

--- src/main/java/com/zju/offercatcher/interfaces/config/WebMvcConfig.java
+++ src/main/java/com/zju/offercatcher/interfaces/config/WebMvcConfig.java
@@ -27,7 +27,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOriginPatterns("http://localhost:*")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
